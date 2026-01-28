b5fce097db96dede63ff00087e09e19455ad249b
floriancse <floriancse@protonmail.com>
2026-01-28 15:28:35 +0100

    Update

--- main.py
+++ main.py
@@ -260,12 +260,6 @@ def get_important_tweets():
             TWEETS
         WHERE
             IMPORTANCE::INT >= 4
-            AND DATE_PUBLISHED::DATE = (
-                SELECT
-                    MAX(DATE_PUBLISHED)::DATE
-                FROM
-                    TWEETS
-            )
         """,
     )
 
